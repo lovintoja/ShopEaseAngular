ecc64a9efef72a13966374a7db035016d2c72286
Filip Dumanowski <filip.dumanowski@qiagen.com>
2025-01-21 10:46:19 +0100

    xxx: stash changes before angular update

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app-routing.module';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(appRoutes),
     provideHttpClient(),
-    importProvidersFrom(ReactiveFormsModule, BrowserAnimationsModule),
+    importProvidersFrom(ReactiveFormsModule, BrowserAnimationsModule), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 }).catch(err => console.error(err));
